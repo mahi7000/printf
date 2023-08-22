2e692b9d5ec8afa48c4ebb5166daa298b385fd8d
mahi7000 <mahlet7000@gmail.com>
2023-08-22 15:03:20 -0700

    is_printable.c

---
+++ is_printable.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * is_printable - if a char is printable
+ * @c: Char.
+ *
+ * Return: 1 if c is printable, 0 otherwise
+ */
+int is_printable(char c)
+{
+	if (c >= 32 && c < 127)
+		return (1);
+
+	return (0);
+}
+
